0e04af20c74b5548d4707a98f6d3014726528271
PrincessVelvet <95100987+ThePrincessVelvet@users.noreply.github.com>
2026-03-30 04:21:57 +0100

    Refactor script.js to improve functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // TODO: Replace with your Supabase Project URL and Anon Key
 const SUPABASE_URL = 'https://wtdutuviptkwdehdpzhu.supabase.co';
-const SUPABASE_KEY = 'sb_secret_dxx0h4eeY3Wqa2pLgMNK-w_gveWpMkG';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0ZHV0dXZpcHRrd2RlaGRwemh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4Mjg4NDQsImV4cCI6MjA5MDQwNDg0NH0.etJwipirdCaTEHS0G10yuBkbNESUark0T8DveeUUO4U';
 
 const _supabase = window.supabase ? supabase.createClient(SUPABASE_URL, SUPABASE_KEY) : null;
 
